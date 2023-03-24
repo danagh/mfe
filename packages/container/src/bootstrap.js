c7a72740024a8d0bb852eebb52d37649f71f487d
Dana Ghrabeti <dana.ghrabeti@sogeti.com>
2023-03-24 08:24:59 +0100

    Remove console.log

--- packages/container/src/bootstrap.js
+++ packages/container/src/bootstrap.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-console.log('container');
-
 ReactDOM.render(
     <App />,
     document.querySelector('#root')
