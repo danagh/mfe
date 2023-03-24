fcf0d1843fe1bc7b897f4786926c4582f430aa97
Dana Ghrabeti <dana.ghrabeti@sogeti.com>
2023-03-24 08:10:47 +0100

    Change

--- packages/container/src/bootstrap.js
+++ packages/container/src/bootstrap.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+console.log('container');
+
 ReactDOM.render(
     <App />,
     document.querySelector('#root')
